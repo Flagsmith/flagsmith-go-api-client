264a396d1f4b23fae1f4e8866d09802b8bdacfce
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-10-14 08:33:35 +0530

    fix tests

--- client_test.go
+++ client_test.go
@@ -438,7 +438,7 @@ func TestGetFeature(t *testing.T) {
 
 const GetMVFeatureOptionResponseJson = `
 {
-    "id": 150,
+    "id": 200,
     "uuid": "8d3512d3-721a-4cae-9855-56c02cb0afe9",
     "type": "unicode",
     "string_value": "option_value_30",
